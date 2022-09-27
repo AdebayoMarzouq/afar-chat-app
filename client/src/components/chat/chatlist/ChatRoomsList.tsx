1ee4620798352c1a6de5e34e2fb330ac9f29ef71
Marzouq <marzouqdevmail@gmail.com>
2022-09-27 21:09:03 +0100

    removed react-window

--- client/src/components/chat/chatlist/ChatRoomsList.tsx
+++ client/src/components/chat/chatlist/ChatRoomsList.tsx
@@ -2,7 +2,6 @@ import React, {memo, useMemo} from "react";
 import { RootState } from "../../../redux/store";
 import { ChatListItem } from "./ChatListItem";
 import { useSelector } from 'react-redux'
-import {FixedSizeList} from 'react-window'
 
 export const ChatRoomsList = () => {
   const { chats } = useSelector((state: RootState) => state.chat)
@@ -10,19 +9,24 @@ export const ChatRoomsList = () => {
   const sorted_chats = useMemo(
     () =>
       [...chats].sort(function (a, b) {
-        return a.updated_at > b.updated_at ? -1 : a.updated_at < b.updated_at ? 1 : 0
+        return a.updated_at > b.updated_at
+          ? -1
+          : a.updated_at < b.updated_at
+          ? 1
+          : 0
       }),
     [chats]
   )
 
+  const ListItem = ({ index }: { index: number; key: string }) => {
+    const chat = sorted_chats[index]
+    return <ChatListItem key={chat.uuid} {...chat} />
+  }
+
   return (
     <div className='pb-4 overflow-y-auto flex-grow'>
       {chats && chats.length ? (
-        <FixedSizeList height={100}  itemCount={sorted_chats.length} itemSize={80} width={100}>
-          {sorted_chats.map((chatItem) => {
-            return <ChatListItem key={chatItem.uuid} {...chatItem} />
-          })}
-        </FixedSizeList>
+        sorted_chats.map(chat => <ChatListItem key={chat.uuid} {...chat} />)
       ) : (
         <div className='text-center'>
           You don not have any chats, create a group or search for some friends
