05bd0010b7d3a3fd4de68a51e577c4fc4a1f165e
Marzouq <marzouqdevmail@gmail.com>
2022-10-02 23:57:37 +0100

    Switched branches

---
+++ client/src/component-copy/ColumnWrapper.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ColumnWrapper = () => {
+  return (
+    <section className='h-screen flex flex-col text-light-text-primary dark:text-dark-text-primary overflow-x-hidden'>
+      ColumnWrapper
+    </section>
+  )
+}
+
+export default ColumnWrapper
--- client/src/components/common/HeaderWrapper.tsx
+++ client/src/components/common/HeaderWrapper.tsx
@@ -6,7 +6,7 @@ export const HeaderWrapper = ({
   children?: React.ReactNode | React.ReactNode[]
 }) => {
   return (
-    <div className='shrink-0 px-4 flex items-center h-16 border-b dark:border-dark-separator bg-inherit dark:bg-dark-bg-secondary text-light-text-primary dark:text-dark-text-primary gap-4'>
+    <div className='test shrink-0 px-4 flex items-center h-16 border-b dark:border-dark-separator bg-inherit dark:bg-dark-bg-secondary text-light-text-primary dark:text-dark-text-primary gap-4'>
       {children}
     </div>
   )
